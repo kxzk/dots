a115e0d28c67593219af852fea4abb439805ab0b
kadekillary <kadekillary@pm.me>
2025-06-12 09:28:53 -0500

    add colors script

--- bin/colors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {0..255}; do
-    printf "\e[48;5;${i}m%4d\e[0m" $i
-    # Print a new line after every 16 colors
-    if (( ($i + 1) % 16 == 0 )); then
-        echo
-    fi
-done
