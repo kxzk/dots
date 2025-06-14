e0833349ca866df0b7b9cbe7529cb2eb9aea40de
kadekillary <kadekillary@pm.me>
2025-06-14 07:15:31 -0500

    update winsplit colo

--- nvim/.config/nvim/lua/configs/colo.lua
+++ nvim/.config/nvim/lua/configs/colo.lua
@@ -1,10 +1,11 @@
 -- my variations to the new neovim default colorscheme
+-- vim.api.nvim_set_hl(0, "Comment", { fg = "#FCE094", italic = true })
 vim.api.nvim_set_hl(0, "Comment", { fg = "#5C6773", italic = true })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#14161b" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1c1d23" })
 vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#4f5258" })
 vim.api.nvim_set_hl(0, "MsgArea", { fg = "#2c2e33" })
-vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#FFCAFF" })
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#FCE094" })
 vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#2c2e33" })
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#2c2e33" })
 -- vim.api.nvim_set_hl(0, "SignColumn", { fg = "#2c2e33", bg = "#1c1d23" })
