93936a6067b76eecd8a91afc869bc37a139e751f
kadekillary <kadekillary@pm.me>
2025-06-22 09:03:14 -0500

    add gruvbox

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
 	{
 		"lewis6991/gitsigns.nvim",
 		opts = {
@@ -175,5 +176,4 @@ return {
 			},
 		},
 	},
-	{ "savq/melange-nvim" },
 }
