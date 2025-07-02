247f36d6ef323df6cde2e7036a1d114029a281b8
kadekillary <kadekillary@pm.me>
2025-07-02 13:21:15 -0500

    update to default

--- nvim/.config/nvim/lua/configs/colo.lua
+++ nvim/.config/nvim/lua/configs/colo.lua
@@ -1,10 +1,29 @@
--- [gruvbox dark]
-vim.api.nvim_set_hl(0, "Comment", { fg = "#504945", italic = true })
-vim.api.nvim_set_hl(0, "CursorLine", { bg = "#32302f" })
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#504945", bg = "#32302f" })
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#3c3836" })
-vim.api.nvim_set_hl(0, "MsgArea", { fg = "#32302f" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#282828" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "#282828" })
-vim.api.nvim_set_hl(0, "TelescopeNormal", { fg = "#3c3836" })
-vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#b8bb26" })
+-- [neovim default dark]
+vim.api.nvim_set_hl(0, "Comment", { fg = "#fce094", italic = true })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#14161b" })
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1c1d23" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#2c2e33" })
+vim.api.nvim_set_hl(0, "MsgArea", { fg = "#2c2e33" })
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#FFCAFF" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#2c2e33" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#2c2e33" })
+vim.api.nvim_set_hl(0, "DiagnosticError", { bg = "#1c1d23" })
+vim.api.nvim_set_hl(0, "DiagnosticWarn", { bg = "#1c1d23" })
+vim.api.nvim_set_hl(0, "DiagnosticInfo", { bg = "#1c1d23" })
+vim.api.nvim_set_hl(0, "DiagnosticHint", { bg = "#1c1d23" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { bg = "#1c1d23", fg = "#2C2E33" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { bg = "#1c1d23", fg = "#FFB454" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { bg = "#1c1d23", fg = "#A3D9FF" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { bg = "#1c1d23", fg = "#2C2E33" })
+-- vim.api.nvim_set_hl(0, "SignColumn", { fg = "#2c2e33", bg = "#1c1d23" })
+
+-- hate those ugly ass green docstrings
+vim.api.nvim_set_hl(0, "@string.documentation.python", {
+	fg = "#5c6773",
+	italic = true,
+})
+
+vim.api.nvim_set_hl(0, "SpellBad", { undercurl = true, sp = "#ff6b6b" })
+vim.api.nvim_set_hl(0, "SpellCap", { undercurl = true, sp = "#e5c07b" })
+vim.api.nvim_set_hl(0, "SpellLocal", { undercurl = true, sp = "#e5c07b" })
+vim.api.nvim_set_hl(0, "SpellRare", { undercurl = true, sp = "#e5c07b" })
