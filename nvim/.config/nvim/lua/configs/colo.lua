85a96f302e0cc7b9b81f59e0e638ed0224fe6b0d
kadekillary <kadekillary@pm.me>
2025-07-21 18:58:49 -0500

    update linenr

--- nvim/.config/nvim/lua/configs/colo.lua
+++ nvim/.config/nvim/lua/configs/colo.lua
@@ -2,6 +2,8 @@ vim.api.nvim_set_hl(0, "@comment", { fg = "#2f334d", italic = true })
 vim.api.nvim_set_hl(0, "MsgArea", { fg = "#2f334d" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#262839" })
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#ff966c" })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#2f334d" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#2f334d" })
 
 -- python
 vim.api.nvim_set_hl(0, "@string.documentation.python", { fg = "#636da6", italic = true })
