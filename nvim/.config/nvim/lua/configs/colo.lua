800ad150e89a2edb67851b20a817c5be7f4e8012
kadekillary <kadekillary@pm.me>
2025-07-21 19:02:26 -0500

    update colo

--- nvim/.config/nvim/lua/configs/colo.lua
+++ nvim/.config/nvim/lua/configs/colo.lua
@@ -2,6 +2,7 @@ vim.api.nvim_set_hl(0, "@comment", { fg = "#2f334d", italic = true })
 vim.api.nvim_set_hl(0, "MsgArea", { fg = "#2f334d" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#262839" })
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#ff966c" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#636da6" })
 vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#2f334d" })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#2f334d" })
 
